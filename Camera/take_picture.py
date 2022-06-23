96a70bf14bd8e410d43aeae98d2d69a7a31c08f6
EyeTeeGuy <74233123+EyeTeeGuy@users.noreply.github.com>
2022-06-23 16:52:48 +0800

    Edited file
    Previous code provided was the wrong one

--- Camera/take_picture.py
+++ Camera/take_picture.py
@@ -1,5 +1,11 @@
-from picamera import PiCamera
-camera = PiCamera()
-time.sleep(2)
-camera.capture("/home/pi/Pictures/img.jpg")
-print("Done.")
+# import libraries
+import time 
+import os 
+
+def take_picture():
+	os.system('sudo fswebcam -r 1280x720 -S 3 --jpeg 50 --save /home/pi/CSC3004/test_image.jpg') # uses Fswebcam to take picture
+	time.sleep(2)
+	print("Photo taken!") 
+	# this line creates a 15 second delay before repeating the loop
+
+take_picture()

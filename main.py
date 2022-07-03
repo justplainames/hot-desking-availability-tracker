21ada0c2feccd3d862110ba45c86e764a62b8425
ohryx <haikalryx@gmail.com>
2022-07-03 18:26:05 +0800

    Added Websites Code

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+from flask import Flask, render_template, request, jsonify, flash, redirect, url_for, session
+import os
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
+app.secret_key = "super secret key"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+camera_images = os.path.join('static', 'images')
+
+app.config['UPLOAD_FOLDER'] = camera_images
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
+
+@app.route('/')
+def home():
+    image_1 = os.path.join(app.config['UPLOAD_FOLDER'], 'test_image2.jpg')
+    seat_1 = 0
+    seat_2 = 1
+
+    return render_template('home.html', user_image=image_1, seat_1=seat_1, seat_2=seat_2)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(debug=True)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+

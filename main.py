419871c9b08171300e4311420c8b739cd17e7423
ohryx <haikalryx@gmail.com>
2022-07-05 19:45:45 +0800

    Integrate AWS with Flask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify, flash, redirect, url_for, session
 import os
+import aws_controller
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
@@ -11,9 +12,6 @@ camera_images = os.path.join('static', 'images')
 app.config['UPLOAD_FOLDER'] = camera_images
 
 
-
-
-
 @app.route('/')
 def home():
     # selected_image_1 =
@@ -21,10 +19,15 @@ def home():
     image_1 = os.path.join(app.config['UPLOAD_FOLDER'], 'test_image2.jpg')
     image_2 = os.path.join(app.config['UPLOAD_FOLDER'], 'test_image3.jpg')
 
-    seat_1 = 0
-    seat_2 = 1
+    seat_1 = 1
+    seat_2 = 5
 
     return render_template('home.html', image_1=image_1, image_2=image_2, seat_1=seat_1, seat_2=seat_2)
+
+
+@app.route('/get-items')
+def get_items():
+    return jsonify(aws_controller.get_items())
 
 
 if __name__ == '__main__':

9667f8ea6bf218cf5a36ce2fd2d79ada6c4b7e8c
ohryx <haikalryx@gmail.com>
2022-07-06 14:52:46 +0800

    remerge

--- aws_controller.py
+++ aws_controller.py
@@ -8,3 +8,8 @@ def get_items():
         TableName='CSC3004'
     )
 
+def get_latest_update():
+    response = dynamo_client.scan(
+        TableName='CSC3004_Table'
+    )
+    return response['Items'][-1]['number_of_People']['N']
